26230c23c501c3d9db5d2245a149ccaebe239e3d
Kristian Dimitrov <krisitoo10@gmail.com>
2024-04-06 15:02:19 +0300

    Initialize backend project

---
+++ src/main/java/com/feelreal/api/FeelRealApplication.java
@@ -0,0 +1,13 @@
+package com.feelreal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelRealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feelreal/api/FeelRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelreal.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
