7364c5bafd14da636340d2a90c215fe87f1b8f0c
Kristian <krisitoo10@gmail.com>
2024-06-18 15:46:05 +0300

    Add article service

--- src/main/java/com/feelreal/api/repository/ArticleRepository.java
+++ src/main/java/com/feelreal/api/repository/ArticleRepository.java
@@ -1,11 +1,16 @@
 package com.feelreal.api.repository;
 
 import com.feelreal.api.model.Article;
+import com.feelreal.api.model.enumeration.ArticleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, UUID> {
+
+    List<Article> findByType(ArticleType type);
+
 }
